3410033306738277e058c4602cc8f13f7522fdf9
LHwang01 <LawrenceHwang01@gmail.com>
2022-02-20 17:50:55 -0500

    Created Ground and Sun

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ ctx.fillStyle = "#ADD8E6";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 //Creates the sky by filling the background color
+
+ctx.fillStyle = "#9b7653";
+ctx.fillRect(0, 135, canvas.width, 100);
+
+ctx.fillStyle = "#348C31";
+ctx.fillRect(0, 135, canvas.width, 2);
+
+//Draws the ground (dirt and grass)
+
+ctx.fillStyle = "yellow";
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 2;
+
+ctx.arc(20, 20, 7, 0, 2 * Math.PI, false);
+ctx.fill();
+ctx.stroke();
+
+//Draws the sun
+
