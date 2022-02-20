d222fec86460364582010586dcb5f276761fcda9
LHwang01 <LawrenceHwang01@gmail.com>
2022-02-20 17:29:23 -0500

    Added Sky (Background Color)

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#ADD8E6";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//Creates the sky by filling the background color
